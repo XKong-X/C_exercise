1441cc24c5c47a6cce8fac44f38c85904d3c3f83
XKong-X <404910156@qq.com>
2023-11-11 11:57:24 +0800

    上午练习

---
+++ test_11.11/test_11.11/test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int sec = 0;
+//	scanf("%d", &sec);
+//	int h = sec / 3600;
+//	int m = (sec % 3600) / 60;
+//	int s = (sec % 3600) % 60;
+//	printf("%d %d %d", h, m, s);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	float score1 = 0.0;
+//	float score2 = 0.0;
+//	float score3 = 0.0;
+//	scanf("%f %f %f", &score1, &score2, &score3);
+//	float sum = score1 + score2 + score3;
+//	float average = sum / 3.0;
+//	printf("%.2f %.2f", sum, average);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	float sum = 0.0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		float s = 0.0;
+//		scanf("%f", &s);
+//		sum += s;
+//	}
+//	printf("%.2f %.2f", sum, sum / 3.0);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int h = 0;
+//	int m = 0;
+//	while (scanf("%d %d %d", &n, &h, &m) != EOF)
+//	{
+//		//if (m % h != 0)
+//		//{
+//			printf("%d", n - (m / h) - (m % h));
+//		//}
+//		/*else
+//		{
+//			printf("%d", n - (m / h));
+//		}*/
+//	}
+//	return 0;
+//}
+//
