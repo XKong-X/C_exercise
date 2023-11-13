16294d20d29434835c2079f0a3ee3792f44525d7
XKong-X <404910156@qq.com>
2023-11-13 12:54:42 +0800

    中午练习

---
+++ test_11.13/test_11.13/test.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//    int s1 = 0;
+//    int s2 = 0;
+//    int s3 = 0;
+//    while (scanf("%d %d %d", &s1, &s2, &s3) != EOF)
+//    {
+//        printf("score1=%d,score2=%d,score3=%d", s1, s2, s3);
+//    }
+//    return 0;
+//}
+//
+//int main()
+//{
+//    int n = 0;
+//    float c = 0.0;
+//    float mat = 0.0;
+//    float eng = 0.0;
+//    while (scanf("%d;%f,%f,%f", &n, &c, &mat, &eng) != EOF)
+//    {
+//        printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", n, c, mat, eng);
+//    }
+//}
+//
+//int main()
+//{
+//    int y = 0;
+//    int m = 0;
+//    int d = 0;
+//    while (scanf("%4d %2d %2d", &y, &m, &d) != EOF)
+//    {
+//        printf("year=%4d\nmonth=%02d\ndate=%02d", y, m, d);
+//    }
+//    return 0;
+//}
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    while (scanf("a=%d,b=%d", &a, &b) != EOF)
+//    {
+//        a = a ^ b;
+//        b = a ^ b;
+//        a = a ^ b;
+//        printf("a=%d,b=%d\n", a, b);
+//        break;
+//    }
+//    return 0;
+//}
+//
+//#include <ctype.h>
+//int main()
+//{
+//    int ch = 0;
+//    while ((ch = getchar()) != EOF)
+//    {
+//        if (islower(ch))
+//        {
+//            printf("%c\n", toupper(ch));
+//        }
+//        else if (isupper(ch))
+//        {
+//            printf("%c\n", tolower(ch));
+//        }
+//    }
+//    return 0;
+}
