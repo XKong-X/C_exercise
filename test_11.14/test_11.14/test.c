f3ed634f89a6e8b16f0a357e7c7a9de5ceb90f2d
XKong-X <404910156@qq.com>
2023-11-14 19:57:52 +0800

    三种方法统计二进制中1的个数

--- test_11.14/test_11.14/test.c
+++ test_11.14/test_11.14/test.c
@@ -145,3 +145,51 @@
 //	return 0;
 //}
 //
+//统计二进制中1的个数(三种方法)
+////1.
+//int count_num_of_1(unsigned int n)
+//{
+//	int count = 0;
+//	while (n != 0)
+//	{
+//		if ((n % 2) == 1)
+//		{
+//			count++;
+//		}
+//		n /= 2;
+//	}
+//	return count;
+//}
+////2.
+//int count_num_of_1(unsigned int n)
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((n >> i) & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+////3.
+//int count_num_of_1(unsigned int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int n = count_num_of_1(num);
+//	printf("%d\n", n);
+//	return 0;
+//}
