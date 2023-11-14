2bf177633c95486d63f3384cda58cf456fd95b92
XKong-X <404910156@qq.com>
2023-11-14 22:53:54 +0800

    练习

--- test_11.14/test_11.14/test.c
+++ test_11.14/test_11.14/test.c
@@ -271,3 +271,24 @@
 //	return 0;
 //}
 //
+#include <math.h>
+int main()
+{
+	int w = 0;
+	int h = 0;
+	double BMI = 0.0;
+	while (~scanf("%d %d", &w, &h))
+	{
+		BMI = w / pow(h * 0.01, 2);
+		if (BMI < 18.5)
+			printf("Underweight\n");
+		else if (BMI >= 18.5 && BMI <= 23.9)
+			printf("Normal\n");
+		else if (BMI > 23.9 && BMI <= 27.9)
+			printf("Overweight\n");
+		else
+			printf("Obese\n");
+	}
+	
+	return 0;
+}
