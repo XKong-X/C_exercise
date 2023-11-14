1d32dd6ee02aa12a7bd40e16929ef769195079e1
XKong-X <404910156@qq.com>
2023-11-14 23:13:55 +0800

    十六进制转十进制

--- test_11.14/test_11.14/test.c
+++ test_11.14/test_11.14/test.c
@@ -271,24 +271,32 @@
 //	return 0;
 //}
 //
-#include <math.h>
-int main()
-{
-	int w = 0;
-	int h = 0;
-	double BMI = 0.0;
-	while (~scanf("%d %d", &w, &h))
-	{
-		BMI = w / pow(h * 0.01, 2);
-		if (BMI < 18.5)
-			printf("Underweight\n");
-		else if (BMI >= 18.5 && BMI <= 23.9)
-			printf("Normal\n");
-		else if (BMI > 23.9 && BMI <= 27.9)
-			printf("Overweight\n");
-		else
-			printf("Obese\n");
-	}
-	
-	return 0;
-}
+//#include <math.h>
+//int main()
+//{
+//	int w = 0;
+//	int h = 0;
+//	double BMI = 0.0;
+//	while (~scanf("%d %d", &w, &h))
+//	{
+//		BMI = w / pow(h * 0.01, 2);
+//		if (BMI < 18.5)
+//			printf("Underweight\n");
+//		else if (BMI >= 18.5 && BMI <= 23.9)
+//			printf("Normal\n");
+//		else if (BMI > 23.9 && BMI <= 27.9)
+//			printf("Overweight\n");
+//		else
+//			printf("Obese\n");
+//	}
+//	
+//	return 0;
+//}
+//
+////十六进制转十进制
+//int main()
+//{
+//    int num = 0xABCDEF;
+//    printf("%15d", num);
+//    return 0;
+//}
