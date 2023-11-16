d95ba6d0b9b7d78aa6ab0f075c493b05f822b25c
XKong-X <404910156@qq.com>
2023-11-16 09:29:31 +0800

    小乐乐排电梯

---
+++ test_11.16/test_11.16/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////–°¿÷¿÷≈≈µÁÃ›
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    if (n < 12)
+//    {
+//        printf("2\n");
+//    }
+//    else
+//    {
+//        printf("%d\n", (4 * (n / 12) + 2));
+//    }
+//    return 0;
+//}
