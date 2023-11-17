9c40c1ecb70f9125de5de487edccfb1db2fe36ad
XKong-X <404910156@qq.com>
2023-11-17 23:07:10 +0800

    三角形判断

--- test_11.17/test_11.17/test.c
+++ test_11.17/test_11.17/test.c
@@ -259,4 +259,35 @@
 //    return 0;
 //}
 //
+////Èý½ÇÐÎÅÐ¶Ï
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    while (~scanf("%d %d %d", &a, &b, &c))
+//    {
+//        if ((a + b > c) && (b + c > a) && (a + c > b))
+//        {
+//            if (a == b && b == c)
+//            {
+//                printf("Equilateral triangle!\n");
+//            }
+//            else if (a == b || b == c || a == c)
+//            {
+//                printf("Isosceles triangle!\n");
+//            }
+//            else
+//            {
+//                printf("Ordinary triangle!\n");
+//            }
+//        }
+//        else
+//        {
+//            printf("Not a triangle!\n");
+//        }
+//    }
+//    return 0;
+//}
+//
 //
