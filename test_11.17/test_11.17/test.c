4503d60e2e42bb3e0d106d7bf8b0a9c26a46586c
XKong-X <404910156@qq.com>
2023-11-17 11:39:30 +0800

    及格分数

--- test_11.17/test_11.17/test.c
+++ test_11.17/test_11.17/test.c
@@ -13,3 +13,17 @@
 //    printf("%.1f", (s1 * 0.2 + s2 * 0.1 + s3 * 0.2 + s4 * 0.5));
 //    return 0;
 //}
+//
+////及格分数
+//int main()
+//{
+//    int s = 0;
+//    while (~scanf("%d", &s))
+//    {
+//        if (s >= 60)
+//            printf("Pass\n");
+//        else
+//            printf("Fail\n");
+//    }
+//    return 0;
+//}
