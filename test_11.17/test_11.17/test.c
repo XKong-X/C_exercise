ed305049cc6a868ca927544287ec418739c588b1
XKong-X <404910156@qq.com>
2023-11-17 11:31:45 +0800

    KiKi算期末成绩

---
+++ test_11.17/test_11.17/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////KiKiËãÆÚÄ©³É¼¨
+//int main()
+//{
+//    int s1 = 0;
+//    int s2 = 0;
+//    int s3 = 0;
+//    int s4 = 0;
+//    scanf("%d %d %d %d", &s1, &s2, &s3, &s4);
+//    printf("%.1f", (s1 * 0.2 + s2 * 0.1 + s3 * 0.2 + s4 * 0.5));
+//    return 0;
+//}
