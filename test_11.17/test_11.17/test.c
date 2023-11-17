d2ce54a9badd2936a4eccdca52d4d1f405b8e495
XKong-X <404910156@qq.com>
2023-11-17 22:45:59 +0800

    HTTP状态码

--- test_11.17/test_11.17/test.c
+++ test_11.17/test_11.17/test.c
@@ -202,3 +202,39 @@
 //    }
 //    return 0;
 //}
+//
+////HTTP×´Ì¬Âë
+//int main()
+//{
+//    int http = 0;
+//    while (~scanf("%d", &http))
+//    {
+//        switch (http)
+//        {
+//        case 200:
+//            printf("OK\n");
+//            break;
+//        case 202:
+//            printf("Accepted\n");
+//            break;
+//        case 400:
+//            printf("Bad Request\n");
+//            break;
+//        case 403:
+//            printf("Forbidden\n");
+//            break;
+//        case 404:
+//            printf("Not Found\n");
+//            break;
+//        case 500:
+//            printf("Internal Server Error\n");
+//            break;
+//        case 502:
+//            printf("Bad Gateway\n");
+//            break;
+//        }
+//    }
+//    return 0;
+//}
+//
+//
