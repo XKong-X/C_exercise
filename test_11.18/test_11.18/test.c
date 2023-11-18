13e8e6b9c5f7e3c7cfb59f4e17c4814cd4e94495
XKong-X <404910156@qq.com>
2023-11-18 13:42:59 +0800

    库函数（strcpy）的最终优化

--- test_11.18/test_11.18/test.c
+++ test_11.18/test_11.18/test.c
@@ -31,5 +31,26 @@
 //	printf("%s\n", arr1);
 //	return 0;
 //}
+////返回char*类型的数值是为了实现链式访问
+////strcpy函数返回的是目标空间的起始地址
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	//断言
+//	assert(src != NULL);
+//	assert(dest != NULL);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[20] = "xxxxxxxxxxxxx";
+//	char arr2[] = "hello bit";
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	return 0;
+//}
 //
 //
