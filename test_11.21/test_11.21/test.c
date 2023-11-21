add0d332c0b8ca82c285e20aa3fe522f18ec0187
XKong-X <404910156@qq.com>
2023-11-21 13:47:15 +0800

    KiKi说祝福语

---
+++ test_11.21/test_11.21/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////KiKiËµ×£¸£Óï
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    while (n--)
+//    {
+//        printf("Happy new year!Good luck!\n");
+//    }
+//    return 0;
+//}
