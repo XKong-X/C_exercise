2264d56669e504d623802228d1de84893df80ea0
XKong-X <404910156@qq.com>
2023-11-22 22:52:13 +0800

    包含数字9的数

---
+++ test_11.22/test_11.22/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+////包含数字9的数
+//int main()
+//{
+//    int count = 0;
+//    int i = 0;
+//    for (i = 1; i <= 2019; i++)
+//    {
+//        int tmp = i;
+//        while (tmp)
+//        {
+//            if (tmp % 10 == 9)
+//            {
+//                count++;
+//                break;
+//            }
+//            else
+//            {
+//                tmp /= 10;
+//            }
+//        }
+//    }
+//    printf("%d", count);
+//    return 0;
+//}
+//
+//
