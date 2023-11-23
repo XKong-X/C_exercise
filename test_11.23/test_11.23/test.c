55efc4d0f4faceda0ea025144df7cb2f21ed05bc
XKong-X <404910156@qq.com>
2023-11-23 20:22:26 +0800

    判断两个数的大小关系

--- test_11.23/test_11.23/test.c
+++ test_11.23/test_11.23/test.c
@@ -83,3 +83,20 @@
 //    }
 //    return 0;
 //}
+//
+////判断两个数的大小关系
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    while (~scanf("%d %d", &a, &b))
+//    {
+//        if (a > b)
+//            printf("%d>%d\n", a, b);
+//        else if (a < b)
+//            printf("%d<%d\n", a, b);
+//        else
+//            printf("%d=%d\n", a, b);
+//    }
+//    return 0;
+//}
