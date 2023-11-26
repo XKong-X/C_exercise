512d1ebdd04a35623b2bf1d603285dcc2e74c0a7
XKong-X <404910156@qq.com>
2023-11-26 16:31:13 +0800

    矩阵元素定位

--- test_11.26/test_11.26/test.c
+++ test_11.26/test_11.26/test.c
@@ -20,21 +20,21 @@
 //    return 0;
 //}
 //
-//矩阵元素定位
-int main()
-{
-    int n, m;
-    scanf("%d%d", &n, &m);
-    int arr[5][5];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            scanf("%d", &arr[i][j]);
-        }
-    }
-    int x, y;
-    scanf("%d%d", &x, &y);
-    printf("%d", arr[x][y]);
-    return 0;
-}
+////矩阵元素定位
+//int main()
+//{
+//    int n, m;
+//    scanf("%d%d", &n, &m);
+//    int arr[5][5];
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j < m; j++)
+//        {
+//            scanf("%d", &arr[i][j]);
+//        }
+//    }
+//    int x, y;
+//    scanf("%d%d", &x, &y);
+//    printf("%d", arr[x][y]);
+//    return 0;
+//}
