8a2a24d7158218cd459a2a07fd3ce41ef766b9f4
XKong-X <404910156@qq.com>
2023-11-27 20:16:08 +0800

    字符串操作

---
+++ test_11.27/test_11.27/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////×Ö·û´®²Ù×÷
+//int main()
+//{
+//    int n, m;
+//    scanf("%d %d", &n, &m);
+//    char str[100];
+//    scanf("%s", str);
+//    for (int i = 0; i < m; i++)
+//    {
+//        int l, r;
+//        char c1, c2;
+//        scanf("%d %d %c %c", &l, &r, &c1, &c2);
+//        for (int j = l - 1; j < r; j++)
+//        {
+//            if (str[j] == c1)
+//            {
+//                str[j] = c2;
+//            }
+//        }
+//    }
+//    printf("%s", str);
+//    return 0;
+//}
