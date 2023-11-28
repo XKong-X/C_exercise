891bcb8b6c70adc154fec43fdb75d21eb15a0e4f
XKong-X <404910156@qq.com>
2023-11-28 23:02:48 +0800

    牛牛的短信

---
+++ test_11.28/test_11.28/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////Å£Å£µÄ¶ÌÐÅ
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int num = 0;
+//    double money = 0.0;
+//    for (int i = n; i > 0; i--)
+//    {
+//        scanf("%d", &num);
+//        money += num > 60 ? 0.2 : 0.1;
+//    }
+//    printf("%.1lf", money);
+//    return 0;
+//}
