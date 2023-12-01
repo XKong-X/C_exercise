bb4557c5876390cb4e1514ce2459e5886dc12e19
XKong-X <404910156@qq.com>
2023-12-01 11:38:16 +0800

    n*n乘法表

---
+++ test_12.1/test_12.1/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////n*n乘法表
+//int main()
+//{
+//	int n;
+//	while (~scanf("%d", &n))
+//	{
+//		for (int i = 1; i <= n; i++)
+//		{
+//			for (int j = 1; j <= i; j++)
+//			{
+//				printf("%02d ", i * j);
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+//
+//最大公约数
+int main()
+{
+	int n, m;
+	while (~scanf("%d", &n, &m))
+	{
+
+	}
+	return 0;
+}
