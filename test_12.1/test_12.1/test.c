8b28d0db951e0ea9ee672b588742844230af578f
XKong-X <404910156@qq.com>
2023-12-01 12:46:36 +0800

    最大公约数

--- test_12.1/test_12.1/test.c
+++ test_12.1/test_12.1/test.c
@@ -20,13 +20,30 @@
 //	return 0;
 //}
 //
-//最大公约数
-int main()
-{
-	int n, m;
-	while (~scanf("%d", &n, &m))
-	{
-
-	}
-	return 0;
-}
+////最大公约数
+//int main()
+//{
+//	int n, m;
+//	while (~scanf("%d %d", &n, &m))
+//	{
+//		////不需要确保n>m,下面算法会自动交换位置
+//		//if (n < m)
+//		//{
+//		//	n = n ^ m;
+//		//	m = n ^ m;
+//		//	n = n ^ m;
+//		//}
+//		//求最大公约数
+//		int tmp;
+//		while (m)
+//		{
+//			tmp = n % m;
+//			n = m;
+//			m = tmp;
+//		}
+//		printf("最大公约数为：%d\n", n);
+//	}
+//	return 0;
+//}
+//
+//
