3de20c92e49020858d3695b83d95e4c0d5f0d869
XKong-X <404910156@qq.com>
2023-12-02 13:15:04 +0800

    空心三角形图案

--- test_12.2/test_12.2/test.c
+++ test_12.2/test_12.2/test.c
@@ -245,3 +245,28 @@
 //    }
 //    return 0;
 //}
+//
+////空心三角形图案
+//int main()
+//{
+//    int n;
+//    while (~scanf("%d", &n))
+//    {
+//        for (int i = 1; i <= n; i++)
+//        {
+//            for (int j = 1; j <= n; j++)
+//            {
+//                if (j == 1 || i == n || i == j)
+//                {
+//                    printf("* ");
+//                }
+//                else
+//                {
+//                    printf("  ");
+//                }
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
