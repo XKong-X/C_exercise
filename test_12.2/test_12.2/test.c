067b269b659422f538a56330f66e064da97a6e07
XKong-X <404910156@qq.com>
2023-12-02 13:20:07 +0800

    数字三角形

--- test_12.2/test_12.2/test.c
+++ test_12.2/test_12.2/test.c
@@ -270,3 +270,21 @@
 //    }
 //    return 0;
 //}
+//
+////Êý×ÖÈý½ÇÐÎ
+//int main()
+//{
+//    int n;
+//    while (~scanf("%d", &n))
+//    {
+//        for (int i = 1; i <= n; i++)
+//        {
+//            for (int j = 1; j <= i; j++)
+//            {
+//                printf("%d ", j);
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
