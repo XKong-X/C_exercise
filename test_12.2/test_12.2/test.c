42ba68f86e99f441ce583171e6bd4682cfe513d5
XKong-X <404910156@qq.com>
2023-12-02 16:01:57 +0800

    圣诞树

--- test_12.2/test_12.2/test.c
+++ test_12.2/test_12.2/test.c
@@ -289,59 +289,59 @@
 //    return 0;
 //}
 //
-//
-int main()
-{
-    int n;
-    while (~scanf("%d", &n))
-    {
-        //控制行数(打印树枝)
-        for (int i = 1; i <= n * 3; i++)
-        {
-            //打印外空格
-            for (int j = n * 3 - i; j > 0; j--)
-            {
-                printf(" ");
-            }
-            //打印小三角和内空格循环次数控制
-            int count = 1;
-            for (int k = 1; k <= i / 3 + 1; k++)
-            {
-                //打印小三角形
-                if (i % 3 == 0 && count < k)//count<k用于解决每个小三角多打印一次小三角底部的问题
-                {
-                    printf("* * * ");
-                    count++;
-                }
-                else
-                {
-                    for (int j = 1; j <= i % 3; j++)
-                    {
-                        printf("* ");
-                    }
-                }
-                //打印内空格
-                if (i % 3 != 0)
-                {
-                    for (int j = 3 - i % 3; j > 0; j--)
-                    {
-                        printf("  ");
-                    }
-                }
-            }
-            //每打印完一行换一次行
-            printf("\n");
-        }
-        //打印树干
-        for (int i = 1; i <= n; i++)
-        {
-            //打印空格
-            for (int j = (n - 1) * 3 + 2; j > 0; j--)
-            {
-                printf(" ");
-            }
-            printf("*\n");
-        }
-    }
-    return 0;
-}
+////圣诞树
+//int main()
+//{
+//    int n;
+//    while (~scanf("%d", &n))
+//    {
+//        //控制行数(打印树枝)
+//        for (int i = 1; i <= n * 3; i++)
+//        {
+//            //打印外空格
+//            for (int j = n * 3 - i; j > 0; j--)
+//            {
+//                printf(" ");
+//            }
+//            //打印小三角和内空格循环次数控制
+//            int count = 1;
+//            for (int k = 1; k <= i / 3 + 1; k++)
+//            {
+//                //打印小三角形
+//                if (i % 3 == 0 && count < k)//count<k用于解决每个小三角多打印一次小三角底部的问题
+//                {
+//                    printf("* * * ");
+//                    count++;
+//                }
+//                else
+//                {
+//                    for (int j = 1; j <= i % 3; j++)
+//                    {
+//                        printf("* ");
+//                    }
+//                }
+//                //打印内空格
+//                if (i % 3 != 0)
+//                {
+//                    for (int j = 3 - i % 3; j > 0; j--)
+//                    {
+//                        printf("  ");
+//                    }
+//                }
+//            }
+//            //每打印完一行换一次行
+//            printf("\n");
+//        }
+//        //打印树干
+//        for (int i = 1; i <= n; i++)
+//        {
+//            //打印空格
+//            for (int j = (n - 1) * 3 + 2; j > 0; j--)
+//            {
+//                printf(" ");
+//            }
+//            printf("*\n");
+//        }
+//    }
+//    return 0;
+//}
