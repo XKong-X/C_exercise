4a628eac5c3109b9fa5995d6a9805f86910197c6
XKong-X <404910156@qq.com>
2023-12-02 11:10:12 +0800

    翻转金字塔图案

--- test_12.2/test_12.2/test.c
+++ test_12.2/test_12.2/test.c
@@ -2,26 +2,52 @@
 
 #include <stdio.h>
 
-//金字塔图案
-int main()
-{
-    int n;
-    while (~scanf("%d", &n))
-    {
-        //控制行数
-        for (int i = 1; i <= n; i++)
-        {
-            //打印空格
-            for (int j = n - i; j > 0; j--)
-            {
-                printf(" ");
-            }
-            for (int j = 0; j < i; j++)
-            {
-                printf("* ");
-            }
-            printf("\n");
-        }
-    }
-    return 0;
-}
+////金字塔图案
+//int main()
+//{
+//    int n;
+//    while (~scanf("%d", &n))
+//    {
+//        //控制行数
+//        for (int i = 1; i <= n; i++)
+//        {
+//            //打印空格
+//            for (int j = n - i; j > 0; j--)
+//            {
+//                printf(" ");
+//            }
+//			  //打印‘*’	
+//            for (int j = 0; j < i; j++)
+//            {
+//                printf("* ");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+//
+////翻转金字塔图案
+//int main()
+//{
+//    int n;
+//    while (~scanf("%d", &n))
+//    {
+//        //控制行数
+//        for (int i = 1; i <= n; i++)
+//        {
+//            //打印空格
+//            for (int j = 1; j < i; j++)
+//            {
+//                printf(" ");
+//            }
+//            //打印‘*’
+//            for (int j = n - i + 1; j > 0; j--)
+//            {
+//                printf("* ");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
