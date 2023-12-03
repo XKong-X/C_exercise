b037a2a325a281f15b377c3b8b714be747383424
XKong-X <404910156@qq.com>
2023-12-03 19:13:55 +0800

    【一维数组】输入10个整数，求平均值

---
+++ test_12.3/test_12.3/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////【一维数组】输入10个整数，求平均值
+//int main()
+//{
+//	int sum = 0;
+//	int arr[10];
+//	for (int i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		sum += arr[i];
+//	}
+//	printf("%lf", sum / 10.0);
+//	return 0;
+//}
