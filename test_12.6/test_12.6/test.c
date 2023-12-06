50fa4758c276f4c0952c88c91a2810df2eeac742
XKong-X <404910156@qq.com>
2023-12-06 19:25:38 +0800

    序列中删除指定数字(只打印，不存储)

---
+++ test_12.6/test_12.6/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////序列中删除指定数字(只打印，不存储)
+//int main()
+//{
+//    int n, del;
+//    scanf("%d", &n);
+//    int arr[50];
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    scanf("%d", &del);
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (arr[i] != del)
+//            printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
