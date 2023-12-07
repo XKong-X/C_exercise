d6d40a8dbdc32d4ca290687fba8e7003970ba464
XKong-X <404910156@qq.com>
2023-12-07 19:28:35 +0800

    喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给n元，可以喝多少汽水

--- test_12.7/test_12.7/test.c
+++ test_12.7/test_12.7/test.c
@@ -17,3 +17,26 @@
 //	printf("%d\n", sum);
 //	return 0;
 //}
+//
+////喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给n元，可以喝多少汽水
+//int main()
+//{
+//	int money;
+//	while (~scanf("%d", &money))
+//	{
+//		int drink = 0;
+//		int empty = 0;
+//		drink = money / 1;
+//		empty = drink / 1;
+//		while (empty >= 2)
+//		{
+//			drink += empty / 2;
+//			empty = empty / 2 + empty % 2;
+//		}
+//		if (money > 0)
+//			printf("%d\n", drink);
+//		else
+//			printf("%d\n", 0);
+//	}
+//	return 0;
+//}
