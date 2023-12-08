6540789f04e95073a8f53c1de993c935585a1d26
XKong-X <404910156@qq.com>
2023-12-08 20:30:24 +0800

    计算一个数的每位之和（递归实现）

--- test_12.8/test_12.8/test.c
+++ test_12.8/test_12.8/test.c
@@ -129,3 +129,26 @@
 //	}
 //	return 0;
 //}
+//
+////计算一个数的每位之和（递归实现）
+//int DigitSum(int n)
+//{
+//	if (n < 10)
+//	{
+//		return n;
+//	}
+//	else
+//	{
+//		return n % 10 + DigitSum(n / 10);
+//	}
+//}
+//int main()
+//{
+//	int n;
+//	while (~scanf("%d", &n))
+//	{
+//		int ret = DigitSum(n);
+//		printf("%d\n", ret);
+//	}
+//	return 0;
+//}
