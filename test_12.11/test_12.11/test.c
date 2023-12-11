a16f5f9e913cfb96bcabca1b0e7f97c0179820a8
XKong-X <404910156@qq.com>
2023-12-11 23:02:03 +0800

    小乐乐查找数字

---
+++ test_12.11/test_12.11/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//–°¿÷¿÷≤È’“ ˝◊÷
+int main()
+{
+    int n, x;
+    scanf("%d", &n);
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &x);
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+            count++;
+    }
+    printf("%d", count);
+    return 0;
+}
