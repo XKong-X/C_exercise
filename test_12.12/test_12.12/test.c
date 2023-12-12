d1c382291fb33389af4b21d1c177f5f6655bf31e
XKong-X <404910156@qq.com>
2023-12-12 18:31:50 +0800

    统计二进制中1的个数

--- test_12.12/test_12.12/test.c
+++ test_12.12/test_12.12/test.c
@@ -2,20 +2,35 @@
 
 #include <stdio.h>
 
-//交换两个变量（不创建临时变量）
-void ExcNum(int* pa, int* pb)
-{
-	(*pa) = (*pa) ^ (*pb);
-	(*pb) = (*pa) ^ (*pb);
-	(*pa) = (*pa) ^ (*pb);
-}
+////交换两个变量（不创建临时变量）
+//void ExcNum(int* pa, int* pb)
+//{
+//	(*pa) = (*pa) ^ (*pb);
+//	(*pb) = (*pa) ^ (*pb);
+//	(*pa) = (*pa) ^ (*pb);
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	ExcNum(&a,&b);
+//	printf("%d %d", a, b);//验证
+//	return 0;
+//}
+//
+//统计二进制中1的个数
 int main()
 {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	ExcNum(&a,&b);
-	printf("%d %d", a, b);//验证
-	return 0;
+    int n;
+    scanf("%d", &n);
+    int m = 1;
+    int count = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        if (n & m)
+            count++;
+        n >>= 1;
+    }
+    printf("%d\n", count);
+    return 0;
 }
-//
-//
