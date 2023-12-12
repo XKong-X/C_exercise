af3d8a6995f9a5a3e43d9f73a54888cebd8a78c8
XKong-X <404910156@qq.com>
2023-12-12 22:19:04 +0800

    打印整数二进制的奇数位和偶数位

--- test_12.12/test_12.12/test.c
+++ test_12.12/test_12.12/test.c
@@ -18,19 +18,40 @@
 //	return 0;
 //}
 //
-//统计二进制中1的个数
+////统计二进制中1的个数
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    int m = 1;
+//    int count = 0;
+//    for (int i = 0; i < 32; i++)
+//    {
+//        if (n & m)
+//            count++;
+//        n >>= 1;
+//    }
+//    printf("%d\n", count);
+//    return 0;
+//}
+//
+//打印整数二进制的奇数位和偶数位
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int m = 1;
-    int count = 0;
-    for (int i = 0; i < 32; i++)
-    {
-        if (n & m)
-            count++;
-        n >>= 1;
-    }
-    printf("%d\n", count);
-    return 0;
+	int n;
+	scanf("%d", &n);
+	int eve_dig = 2;
+	int odd_dig = 1;
+	int count_eve = 0;
+	int count_odd = 0;
+	for (int i = 0; i < 16; i++)
+	{
+		if (n & eve_dig)
+			count_eve++;
+		if (n & odd_dig)
+			count_odd++;
+		n >>= 2;
+	}
+	printf("偶数位有%d个 奇数位有%d个\n", count_eve, count_odd);
+	return 0;
 }
