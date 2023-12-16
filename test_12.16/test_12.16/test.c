9f1e553252f5c9cefeef405a69a7cf78402fe2c3
XKong-X <404910156@qq.com>
2023-12-16 23:54:41 +0800

    牛牛的素数和

---
+++ test_12.16/test_12.16/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//Å£Å£µÄËØÊýºÍ
+int is_prime(int n)
+{
+    int i = 0;
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int l, r;
+    scanf("%d %d", &l, &r);
+    int sum = 0;
+    for (int i = l; i <= r; i++)
+    {
+        if (is_prime(i))
+            sum += i;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
