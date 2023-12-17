d17c711547edd49e60436109402a899fe92be6bf
XKong-X <404910156@qq.com>
2023-12-17 21:29:23 +0800

    猜凶手

--- test_12.17/test_12.17/test.c
+++ test_12.17/test_12.17/test.c
@@ -2,32 +2,45 @@
 
 #include <stdio.h>
 
-//猜名次
+////猜名次
+//int main()
+//{
+//	int a, b, c, d, e;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (((b == 2) + (a == 3) == 1) &&
+//							((b == 2) + (e == 4) == 1) &&
+//							((c == 1) + (d == 2) == 1) &&
+//							((c == 5) + (d == 3) == 1) &&
+//							((e == 4) + (a == 1) == 1))
+//						{
+//							if (a * b * c * d * e == 120)
+//								printf("a=%d b=%d c=%d d=%d e=%d\n", a, b, c, d, e);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
+//
+//猜凶手
 int main()
 {
-	int a, b, c, d, e;
-	for (a = 1; a <= 5; a++)
+	for (int killer = 'a'; killer <= 'd'; killer++)
 	{
-		for (b = 1; b <= 5; b++)
+		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
 		{
-			for (c = 1; c <= 5; c++)
-			{
-				for (d = 1; d <= 5; d++)
-				{
-					for (e = 1; e <= 5; e++)
-					{
-						if (((b == 2) + (a == 3) == 1) &&
-							((b == 2) + (e == 4) == 1) &&
-							((c == 1) + (d == 2) == 1) &&
-							((c == 5) + (d == 3) == 1) &&
-							((e == 4) + (a == 1) == 1))
-						{
-							if (a * b * c * d * e == 120)
-								printf("a=%d b=%d c=%d d=%d e=%d\n", a, b, c, d, e);
-						}
-					}
-				}
-			}
+			printf("%c", killer);
 		}
 	}
 	return 0;
