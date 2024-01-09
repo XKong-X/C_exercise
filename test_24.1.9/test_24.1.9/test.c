8d1225aecbf6617f922a7b7ca96a4b7658456f8c
XKong-X <404910156@qq.com>
2024-01-10 00:16:01 +0800

    库函数strstr的模拟实现

--- test_24.1.9/test_24.1.9/test.c
+++ test_24.1.9/test_24.1.9/test.c
@@ -43,7 +43,7 @@
 //int my_strcmp(const char* str1, const char* str2)//加const表示，只比较* str1和* str2，不进行修改
 //{
 //	assert(str1 && str2);//断言
-//	while (str1 && str2 && *str1 == *str2)//当有指针指向‘\0’或者指向的字符不相等时，停止循环
+//	while (*str1 && *str2 && *str1 == *str2)//当有指针指向‘\0’或者指向的字符不相等时，停止循环
 //	{
 //		str1++;
 //		str2++;
@@ -58,20 +58,54 @@
 //	return 0;
 //}
 //
-//库函数strcat的模拟实现
-char* my_strcat(char* dest, const char* src)
+////库函数strcat的模拟实现
+//char* my_strcat(char* dest, const char* src)
+//{
+//	assert(dest && src);//断言
+//	char* ret = dest;//保存dest最初指向的地址
+//	while (*dest)//找dest的‘\0’字符
+//		dest++;
+//	while(*dest++ = *src++);//将src中的字符追加到dest中,包括‘\0’
+//	return ret;
+//}
+//int main()
+//{
+//	char str1[20] = "Hello ";
+//	char str2[] = "World";
+//	printf("%s\n", my_strcat(str1, str2));
+//	return 0;
+//}
+//
+//库函数strstr的模拟实现
+char* my_strstr(char* str1, char* str2)
 {
-	assert(dest && src);//断言
-	char* ret = dest;//保存dest最初指向的地址
-	while (*dest)//找dest的‘\0’字符
-		dest++;
-	while(*dest++ = *src++);//将src中的字符追加到dest中,包括‘\0’
-	return ret;
+	assert(str1 && str2);
+	char* cp = str1;//匹配失败时应该回到的位置
+	char* s1 = cp;//str1用来匹配的字符的地址
+	char* s2 = str2;//str2用来匹配的字符的地址
+	while (*cp)//当cp指向'\0'时，停止循环
+	{
+		//开始匹配
+		while (*s1 && *s2 && *s1 == *s2)//当有指针指向‘\0’或者指向的字符不相等时，停止循环
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')//当s2指向'\0'时说明匹配成功，返回cp的值
+			return cp;
+		s1 = ++cp;//cp+1并将值赋给s1（重置s1的值）
+		s2 = str2;
+	}
+	return NULL;//匹配失败返回NULL
 }
-int main()
+int main() 
 {
-	char str1[20] = "Hello ";
-	char str2[] = "World";
-	printf("%s\n", my_strcat(str1, str2));
+	char str1[] = "abbcdef";
+	char str2[] = "bbc";
+	char* ret = my_strstr(str1, str2);
+	if (ret != NULL)
+		printf("%s\n", ret);
+	else
+		printf("找不到！\n");
 	return 0;
 }
