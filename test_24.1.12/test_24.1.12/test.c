31c7a4fe55e3d0c7f1b52138fd7dc6aa95faf96c
XKong-X <404910156@qq.com>
2024-01-12 23:03:47 +0800

    BC135 图像相似度

--- test_24.1.12/test_24.1.12/test.c
+++ test_24.1.12/test_24.1.12/test.c
@@ -20,41 +20,74 @@
 //    return 0;
 //}
 //
-//BC133 回型矩阵
+////BC133 回型矩阵
+//int main()
+//{
+//    int n;
+//    scanf("%d",&n);
+//    int arr[20][20];
+//    int up = 0, down = n - 1, left = 0, right = n - 1;
+//    int num = 1;
+//    while (num <= n * n)
+//    {
+//        int k;
+//        //上边的边界
+//        for (k = left; k <= right; k++)
+//            arr[up][k] = num++;
+//        up++;
+//        //右边的边界
+//        for (k = up; k <= down; k++)
+//            arr[k][right] = num++;
+//        right--;
+//        //下边的边界
+//        for (k = right; k >= left; k--)
+//            arr[down][k] = num++;
+//        down--;
+//        //左边的边界
+//        for (k = down; k >= up; k--)
+//            arr[k][left] = num++;
+//        left++;
+//    }
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j < n; j++)
+//        {
+//            printf("%-3d ", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//
+//BC135 图像相似度
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    int arr[20][20];
-    int up = 0, down = n - 1, left = 0, right = n - 1;
-    int num = 1;
-    while (num <= n * n)
-    {
-        int k;
-        //上边的边界
-        for (k = left; k <= right; k++)
-            arr[up][k] = num++;
-        up++;
-        //右边的边界
-        for (k = up; k <= down; k++)
-            arr[k][right] = num++;
-        right--;
-        //下边的边界
-        for (k = right; k >= left; k--)
-            arr[down][k] = num++;
-        down--;
-        //左边的边界
-        for (k = down; k >= up; k--)
-            arr[k][left] = num++;
-        left++;
-    }
-    for (int i = 0; i < n; i++)
+    int m, n;
+    scanf("%d %d", &m, &n);
+    int arr1[100][100], arr2[100][100];
+    for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            printf("%-3d ", arr[i][j]);
+            scanf("%d", &arr1[i][j]);
         }
-        printf("\n");
     }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr2[i][j]);
+        }
+    }
+    double count = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr1[i][j] == arr2[i][j])
+                count++;
+        }
+    }
+    printf("%.2lf", count / (m * n) * 100);
     return 0;
 }
