62d5fa1473c2a16fb8d92d35c2470dd6afd735f7
XKong-X <404910156@qq.com>
2024-01-13 13:57:08 +0800

    BC137 序列重组矩阵

---
+++ test_24.1.13/test_24.1.13/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//BC137 序列重组矩阵
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr[10][10];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            //读一个数据马上打印，可以少写一个循环
+            scanf("%d", &arr[i][j]);
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
