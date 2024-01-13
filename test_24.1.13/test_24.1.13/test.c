7cdde1803d9c1e891dfa8563e27c9f7ee929da3f
XKong-X <404910156@qq.com>
2024-01-13 14:43:55 +0800

    BC138 矩阵转置

--- test_24.1.13/test_24.1.13/test.c
+++ test_24.1.13/test_24.1.13/test.c
@@ -2,7 +2,26 @@
 
 #include <stdio.h>
 
-//BC137 序列重组矩阵
+////BC137 序列重组矩阵
+//int main()
+//{
+//    int n, m;
+//    scanf("%d %d", &n, &m);
+//    int arr[10][10];
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j < m; j++)
+//        {
+//            //读一个数据马上打印，可以少写一个循环
+//            scanf("%d", &arr[i][j]);
+//            printf("%d ", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//
+//BC138 矩阵转置
 int main()
 {
     int n, m;
@@ -12,9 +31,14 @@ int main()
     {
         for (int j = 0; j < m; j++)
         {
-            //读一个数据马上打印，可以少写一个循环
             scanf("%d", &arr[i][j]);
-            printf("%d ", arr[i][j]);
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", arr[j][i]);
         }
         printf("\n");
     }
