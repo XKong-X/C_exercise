d7c2e702e1491af1907ac0a60b55516e03abaa86
XKong-X <404910156@qq.com>
2024-01-13 22:57:48 +0800

    BC157 素数回文

--- test_24.1.13/test_24.1.13/test.c
+++ test_24.1.13/test_24.1.13/test.c
@@ -100,32 +100,122 @@
 //    return 0;
 //}
 //
-//BC140 杨辉三角
+////BC140 杨辉三角
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    int arr[30][30] = { 0 };
+//
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j <= i; j++)
+//        {
+//            if (j == 0 || i == j)//处理边上数据
+//                arr[i][j] = 1;
+//            if (i >= 2 && j >= 1)//处理中间数据
+//                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//        }
+//    }
+//
+//    //打印
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j <= i; j++)
+//        {
+//            printf("%5d", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//
+////BC142 扫雷
+//size_t MineNum(const char board[1002][1002], const int  i, const int j)
+//{
+//    int count = 0;
+//    for (int x = i - 1; x <= i + 1; x++)//限制横坐标
+//    {
+//        for (int y = j - 1; y <= j +1; y++)//限制纵坐标
+//        {
+//            if (board[x][y] == '*')
+//                count++;
+//        }
+//    }
+//    return count;
+//}
+//int main()
+//{
+//    int n, m;
+//    scanf("%d %d", &n, &m);
+//    char board[1002][1002] = { 0 };//多创建两行和两列
+//
+//    for (int i = 1; i <= n; i++)//空出最上和最下两行不使用，防止访问时越界
+//    {
+//        getchar();//处理缓存区残留的\n
+//        for (int j = 1; j <= m; j++)//空出最左和最右两列不使用，防止访问时越界
+//        {
+//            scanf("%c", &board[i][j]);
+//        }
+//    }
+//
+//    for (int i = 1; i <= n; i++)
+//    {
+//        for (int j = 1; j <= m; j++)
+//        {
+//            if(board[i][j] == '.')
+//                board[i][j]=MineNum(board, i, j) + '0';
+//        }
+//    }
+//    
+//    for (int i = 1; i <= n; i++)
+//    {
+//        for (int j = 1; j <= m; j++)
+//        {
+//            printf("%c", board[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//
+//BC157 素数回文
+#include <math.h>
+int IsPriNum(int num)
+{
+    int flag = 1;//假设是素数
+    int tmp = num;
+    int dig = 0;
+    
+    while (tmp)//求位数
+    {
+        tmp /= 10;
+        dig++;
+    }
+    
+    long long sum = num * pow(10, --dig);//4位数*1000，3位数*100...
+    tmp = num / 10;//去掉不用变的个位数（变回文数后在中间）
+    while (dig)//求回文数
+    {
+        sum += (tmp % 10) * pow(10, --dig);
+        tmp /= 10;
+    }
+
+    //判断回文数是否为素数
+    for (int i = 2; i <= sqrt(sum); i++)
+    {
+        if (sum % i == 0)
+            return 0;
+    }
+    return flag;
+}
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int arr[30][30] = { 0 };
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            if (j == 0 || i == j)//处理边上数据
-                arr[i][j] = 1;
-            if (i >= 2 && j >= 1)//处理中间数据
-                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-        }
-    }
-
-    //打印
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            printf("%5d", arr[i][j]);
-        }
-        printf("\n");
-    }
+    int pri_num;
+    scanf("%d", &pri_num);
+    if (IsPriNum(pri_num))
+        printf("prime\n");
+    else
+        printf("noprime\n");
     return 0;
 }
