57300fce262760b534112d65605d5664b41d4d12
XKong-X <404910156@qq.com>
2024-01-14 12:28:29 +0800

    BC163 牛牛的替换

--- test_24.1.14/test_24.1.14/test.c
+++ test_24.1.14/test_24.1.14/test.c
@@ -2,28 +2,56 @@
 
 #include <stdio.h>
 
-//BC159 兔子的序列
-#include <math.h>
-int SeqName(const int arr[], const int n)
-{
-    int name = arr[0];
-    for (int i = 0; i < n; i++)
-    {
-        if (((int)sqrt(arr[i]) != arr[i] / (int)sqrt(arr[i])) && name < arr[i])
-            name = arr[i];
-    }
-    return name;
-}
+////BC159 兔子的序列
+//#include <math.h>
+//int SeqName(const int arr[], const int n)
+//{
+//    int name = arr[0];
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (((int)sqrt(arr[i]) != arr[i] / (int)sqrt(arr[i])) && name < arr[i])
+//            name = arr[i];
+//    }
+//    return name;
+//}
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    int arr[1000];
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//
+//    }
+//    printf("%d", SeqName(arr, n));
+//    return 0;
+//}
+//
+//BC163 牛牛的替换
 int main()
 {
     int n;
-    scanf("%d", &n);
-    int arr[1000];
+    char a1, a2, a3, a4;
+    scanf("%d %c %c %c %c", &n, &a1, &a2, &a3, &a4);
+    getchar();//处理缓存区的\n
+    char str[100] = { 0 };
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &arr[i]);
-
+        str[i] = getchar();
+        if (str[i] == a1)
+        {
+            if (a2 == a3)//当找到a1且a2=a3时，直接赋值为a4
+            {
+                str[i] = a4;
+                break;
+            }
+            else
+                str[i] = a2;
+        }
+        else if (str[i] == a3)
+            str[i] = a4;
     }
-    printf("%d", SeqName(arr, n));
+    printf("%s", str);
     return 0;
 }
