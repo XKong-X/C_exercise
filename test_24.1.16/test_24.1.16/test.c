af904e5b8451a8419b63f537bd066bca5696651d
XKong-X <404910156@qq.com>
2024-01-16 13:42:24 +0800

    BC125 序列中整数去重

--- test_24.1.16/test_24.1.16/test.c
+++ test_24.1.16/test_24.1.16/test.c
@@ -19,19 +19,50 @@
 //    return 0;
 //}
 //
-//BC173 牛牛逆序输出
-void fun(int n)
+////BC173 牛牛逆序输出
+//void fun(int n)
+//{
+//    if (n > 0)
+//    {
+//        printf("%d", n % 10);
+//        return fun(n / 10);
+//    }
+//}
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    fun(n);
+//    return 0;
+//}
+//
+//BC125 序列中整数去重
+void fun(int arr[], int n)
 {
-    if (n > 0)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d", n % 10);
-        return fun(n / 10);
+        int flag = 1;//默认需要打印
+        for (int j = 0; j < i; j++)//对比一下前面的数字是否有相等的
+        {
+            if (arr[i] == arr[j])//相等则将flag的值赋为0（不打印），并跳出当前循环
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag)
+            printf("%d ", arr[i]);
     }
 }
 int main()
 {
     int n;
     scanf("%d", &n);
-    fun(n);
+    int arr[5000] = { 0 };
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    fun(arr, n);
     return 0;
 }
