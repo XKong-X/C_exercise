031c7a6d0244e65bd771708c3a23e9db922c9e95
XKong-X <404910156@qq.com>
2024-01-16 11:15:56 +0800

    BC173 牛牛逆序输出

--- test_24.1.16/test_24.1.16/test.c
+++ test_24.1.16/test_24.1.16/test.c
@@ -2,19 +2,36 @@
 
 #include <stdio.h>
 
-//BC172 牛牛的排列数
-int fun(int n, int tmp)
+////BC172 牛牛的排列数
+//int fun(int n, int tmp)
+//{
+//    if (n < tmp)
+//        return 1;
+//    else
+//        return n * fun(n - 1, tmp);
+//}
+//int main()
+//{
+//    int n, m;
+//    scanf("%d %d", &n, &m);
+//    int tmp = n - m + 1;
+//    printf("%d", fun(n, tmp));
+//    return 0;
+//}
+//
+//BC173 牛牛逆序输出
+void fun(int n)
 {
-    if (n < tmp)
-        return 1;
-    else
-        return n * fun(n - 1, tmp);
+    if (n > 0)
+    {
+        printf("%d", n % 10);
+        return fun(n / 10);
+    }
 }
 int main()
 {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    int tmp = n - m + 1;
-    printf("%d", fun(n, tmp));
+    int n;
+    scanf("%d", &n);
+    fun(n);
     return 0;
 }
