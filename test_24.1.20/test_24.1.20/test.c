99eb10c8f558bc314b70a429a0edcbcac45e35fd
XKong-X <404910156@qq.com>
2024-01-20 23:04:20 +0800

    7-5 表格输出（傻波一题目）

--- test_24.1.20/test_24.1.20/test.c
+++ test_24.1.20/test_24.1.20/test.c
@@ -2,16 +2,40 @@
 
 #include <stdio.h>
 
-//7-4 BCD解密
+////7-4 BCD解密
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    if (!n)
+//    {
+//        printf("%d", 0);
+//        return 0;
+//    }
+//    printf("%d%d", n / 16, n % 16);
+//    return 0;
+//}
+//
+//7-5 表格输出（傻波一题目）
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    if (!n)
-    {
-        printf("%d", 0);
-        return 0;
-    }
-    printf("%d%d", n / 16, n % 16);
+    /*printf("------------------------------------\n");
+    printf("%-14s%9s   %-9s\n", "Province", "Area(km2)", "Pop.(10K)");
+    printf("------------------------------------\n");
+    printf("%-14s%9.2lf   %-9.2lf\n", "Anhui", 139600.00, 6461.00);
+    printf("%-14s%9.2lf   %-9.2lf\n", "Beijing", 16410.54, 1180.70);
+    printf("%-14s%9.2lf   %-9.2lf\n", "Chongqing", 82400.00, 3144.23);
+    printf("%-14s%9.2lf   %-9.2lf\n", "Shanghai", 6340.50, 1360.26);
+    printf("%-14s%9.2lf   %-9.2lf\n", "Zhejiang", 101800.00, 4894.00);
+    printf("------------------------------------");*/
+    printf("------------------------------------\n");
+    printf("Province      Area(km2)   Pop.(10K)\n");
+    printf("------------------------------------\n");
+    printf("Anhui         139600.00   6461.00\n");
+    printf("Beijing        16410.54   1180.70\n");
+    printf("Chongqing      82400.00   3144.23\n");
+    printf("Shanghai        6340.50   1360.26\n");
+    printf("Zhejiang      101800.00   4894.00\n");
+    printf("------------------------------------");//最后一行不要加\n，否则会空一行 
     return 0;
 }
