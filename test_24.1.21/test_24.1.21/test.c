472fd4f08e03893ec77ad7215684095241c98b9a
XKong-X <404910156@qq.com>
2024-01-21 12:11:08 +0800

    7-12 两个数的简单计算器

--- test_24.1.21/test_24.1.21/test.c
+++ test_24.1.21/test_24.1.21/test.c
@@ -2,14 +2,35 @@
 
 #include <stdio.h>
 
-//7-11 分段计算居民水费
+////7-11 分段计算居民水费
+//int main()
+//{
+//    int x;
+//    scanf("%d", &x);
+//    if (x <= 15)
+//        printf("%.2f", 4.0 * x / 3.0);
+//    else
+//        printf("%.2f", 2.5 * x - 17.5);
+//    return 0;
+//}
+//
+//7-12 两个数的简单计算器
 int main()
 {
-    int x;
-    scanf("%d", &x);
-    if (x <= 15)
-        printf("%.2f", 4.0 * x / 3.0);
+    int num1, num2;
+    char sign;
+    scanf("%d %c %d", &num1, &sign, &num2);
+    if (sign == '+')
+        printf("%d", num1 + num2);
+    else if (sign == '-')
+        printf("%d", num1 - num2);
+    else if (sign == '*')
+        printf("%d", num1 * num2);
+    else if (sign == '/')
+        printf("%d", num1 / num2);
+    else if (sign == '%')
+        printf("%d", num1 % num2);
     else
-        printf("%.2f", 2.5 * x - 17.5);
+        printf("ERROR");
     return 0;
 }
