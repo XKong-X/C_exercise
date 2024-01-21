7e0a40c2c10054fb2f2dac477b063884641c5840
XKong-X <404910156@qq.com>
2024-01-21 12:56:12 +0800

    7-13 日K蜡烛图

--- test_24.1.21/test_24.1.21/test.c
+++ test_24.1.21/test_24.1.21/test.c
@@ -14,23 +14,45 @@
 //    return 0;
 //}
 //
-//7-12 两个数的简单计算器
+////7-12 两个数的简单计算器
+//int main()
+//{
+//    int num1, num2;
+//    char sign;
+//    scanf("%d %c %d", &num1, &sign, &num2);
+//    if (sign == '+')
+//        printf("%d", num1 + num2);
+//    else if (sign == '-')
+//        printf("%d", num1 - num2);
+//    else if (sign == '*')
+//        printf("%d", num1 * num2);
+//    else if (sign == '/')
+//        printf("%d", num1 / num2);
+//    else if (sign == '%')
+//        printf("%d", num1 % num2);
+//    else
+//        printf("ERROR");
+//    return 0;
+//}
+//
+//7-13 日K蜡烛图
 int main()
 {
-    int num1, num2;
-    char sign;
-    scanf("%d %c %d", &num1, &sign, &num2);
-    if (sign == '+')
-        printf("%d", num1 + num2);
-    else if (sign == '-')
-        printf("%d", num1 - num2);
-    else if (sign == '*')
-        printf("%d", num1 * num2);
-    else if (sign == '/')
-        printf("%d", num1 / num2);
-    else if (sign == '%')
-        printf("%d", num1 % num2);
+    double O, H, L, C;
+    scanf("%lf %lf %lf %lf", &O, &H, &L, &C);
+    //蜡烛类型
+    if (C < O)
+        printf("%s", "BW-Solid");
+    else if (C > O)
+        printf("%s", "R-Hollow");
     else
-        printf("ERROR");
+        printf("%s", "R-Cross");
+    //影线
+    if (L<O && L<C && H>O && H>C)
+        printf("%s", " with Lower Shadow and Upper Shadow");
+    else if (L < O && L < C)
+        printf("%s", " with Lower Shadow");
+    else if (H > O && H > C)
+        printf("%s", " with Upper Shadow");
     return 0;
 }
